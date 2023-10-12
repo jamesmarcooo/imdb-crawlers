879c08282ed8b667db85c2045bdaacef11e91268
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-12 17:08:06 +0800

    Scrape specific movie properties

---
+++ imdb/imdb/spiders/best_movies.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = "best_movies"
+    allowed_domains = ["web.archive.org"]
+    start_urls = ["https://web.archive.org/web/20200715000935if_/https://www.imdb.com/search/title/?groups=top_250&sort=user_rating"]
+
+    rules = (Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback="parse_item", follow=True),)
+
+    def parse_item(self, response):
+        yield{
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("normalize-space((//time)[1]/text())").get(), #normalize-space to remove space and /n
+            'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url': response.url
+        }
